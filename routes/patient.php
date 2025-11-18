8b64b15e4cb5c935e32bd4f741beb0dda01b122d
Clyde Escobidal <cleidoscope@gmail.com>
2025-11-18 20:52:12 +0800

    WIP

--- routes/patient.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\PatientController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth')->group(function () {
-    Route::get('patients/{id}', [PatientController::class, 'info'])->name('patients.info');
-});
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
-
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('patients', function () {
-    return Inertia::render('patients/List');
-})->middleware(['auth', 'verified'])->name('patients');
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
