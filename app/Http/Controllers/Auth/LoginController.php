78848543cdf65edb69ccc68c9fa088d020a65afb
Clyde Escobidal <cleidoscope@gmail.com>
2025-11-18 21:51:20 +0800

    WIP

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\TokenResource;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request) {
+        $user = User::where('email', $request->input('email'))->firstOrFail();
+        
+        if (! Hash::check($request->input('password'), $user->password)) {
+            abort(422, "INVALID_PASSWORD");
+        }
+
+        $tokenName = sprintf("%s %s", $request->header('User-Agent'), $request->ip());
+        $token = $user->createToken($tokenName);
+
+        return TokenResource::make(['token' => $token->plainTextToken]);
+    }
+}
